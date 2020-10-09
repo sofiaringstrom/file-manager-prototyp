2859344d68e2974dea6fb6aab60a271a78edf9a4
Sofia Ringström <sofia.ringstrom@live.se>
2020-10-09 16:06:43 +0200

    Updates

---
+++ precache-manifest.8ba06a7d1e28f1b08a40ff5d24d030c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86bac2d6ae83ec325290",
+    "url": "/file-manager-prototyp/static/css/main.abddb763.chunk.css"
+  },
+  {
+    "revision": "86bac2d6ae83ec325290",
+    "url": "/file-manager-prototyp/static/js/main.86bac2d6.chunk.js"
+  },
+  {
+    "revision": "f0ba35c77d7019567fe0",
+    "url": "/file-manager-prototyp/static/js/1.f0ba35c7.chunk.js"
+  },
+  {
+    "revision": "259ee48e40d1a37572ff",
+    "url": "/file-manager-prototyp/static/js/runtime~main.259ee48e.js"
+  },
+  {
+    "revision": "434f33bc79c23dc863f02904457e0d87",
+    "url": "/file-manager-prototyp/index.html"
+  }
+];
